fa2361c754eb363b64319869890ba7b8fd8793bc
Wurzsag <elias.pohjalainen@cs.tamk.fi>
2019-04-15 06:48:19 +0300

    MainActivity add timer options

--- app/src/main/java/fi/tuni/worktimetracker/MainActivity.java
+++ app/src/main/java/fi/tuni/worktimetracker/MainActivity.java
@@ -8,13 +8,19 @@ import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
     private TextView tvTimer;
+    private TimePicker timePicker;
 
     long startTime;
+    long currentRunTime;
+    long stopTime;
     boolean running;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
@@ -44,23 +50,24 @@ public class MainActivity extends AppCompatActivity {
 
         mTextMessage = (TextView) findViewById(R.id.message);
         tvTimer = findViewById(R.id.timer);
+        timePicker = (TimePicker) findViewById(R.id.timePickerStop);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        timePicker.setIs24HourView(true);
     }
 
     public void updateText() {
 
-        runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                long mSeconds = System.currentTimeMillis() - startTime;
-                int seconds = (int) (mSeconds / 1000) % 60;
-                int minutes = (int) (mSeconds / 1000) / 60;
-                int hours = minutes / 60;
+        runOnUiThread(() -> {
+            long mSeconds = System.currentTimeMillis() - startTime;
+            currentRunTime = mSeconds;
 
-                //tvTimer.setText(Long.toString(System.currentTimeMillis() - startTime));
-                tvTimer.setText(String.format("%02d:%02d:%02d", hours, minutes, seconds));
-            }
+            int seconds = (int) (mSeconds / 1000) % 60;
+            int minutes = (int) (mSeconds / 1000) / 60 % 60;
+            int hours = (int) (mSeconds / 1000) / 60 / 60;
+
+            tvTimer.setText(String.format("%02d:%02d:%02d", hours, minutes, seconds));
         });
     }
 
@@ -69,12 +76,17 @@ public class MainActivity extends AppCompatActivity {
         running = true;
 
         while(running) {
+
+            if(stopTime < System.currentTimeMillis())
+                running = false;
+
+            updateText();
+
             try {
                 Thread.sleep(1000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            updateText();
         }
     }
 
@@ -83,11 +95,40 @@ public class MainActivity extends AppCompatActivity {
 
         Thread t = new Thread(this::runTimer);
         t.start();
-
     }
 
     public void stopTimer(View v) {
         running = false;
+    }
+
+    public void addStopTime(View v) {
+        timePicker = (TimePicker) findViewById(R.id.timePickerStop);
+
+        int pickerHour = timePicker.getHour();
+        int pickerMinute = timePicker.getMinute();
+
+        Calendar calendar = Calendar.getInstance();
+        int currentHour = calendar.get(Calendar.HOUR_OF_DAY);
+        int currentMinute = calendar.get(Calendar.MINUTE);
+
+        long stopTimeMinutes = (pickerHour - currentHour) * 60 + (pickerMinute - currentMinute);
+
+        stopTime = System.currentTimeMillis() + stopTimeMinutes * 60 * 1000;
+    }
+
+    public void continueTimer(View v) {
+        startTime = System.currentTimeMillis() - currentRunTime;
+
+        Thread t = new Thread(this::runTimer);
+        t.start();
+    }
+
+    public void addMinutes(View v) {
+        startTime -= 600000;
+    }
+
+    public void addHour(View v) {
+        startTime -= 3600000;
     }
 
 }
